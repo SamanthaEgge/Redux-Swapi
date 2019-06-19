48513008e73d71bb9ad61841314f9aaeb6f449c8
unknown <samantha.c.egge@gmail.com>
2019-06-19 12:30:56 -0700

    Installed yarn to app. Initial set-up for reducers and actions for fetching characters.

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,10 +1,18 @@
-import /* we need our action types here*/ "../actions";
+import { CHARACTER_FETCH, CHARACTER_FETCH_SUCCESS, CHARACTER_FETCH_FAILURE } from "../actions";
+
 const initialState = {
-  characters: []
+  characters: [],
+  isFetching: false,
+  error: '',
   // Array characters, Boolean fetching, null error.
 };
+
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case CHARACTER_FETCH:
+      return {
+
+      }
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
